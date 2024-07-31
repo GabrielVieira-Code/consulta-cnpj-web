59bc4164a4f4ae7df4a2963530c1307534f6f640
gabrielvieira-code <gabrielvieira.code@gmail.com>
2024-07-31 20:21:47 -0300

    componentes finalizados

--- src/pages/home/stiled.ts
+++ src/pages/home/stiled.ts
@@ -1,10 +1,9 @@
-// src/pages/home/styled.ts
 import styled from 'styled-components';
 import { FaShoppingCart } from 'react-icons/fa';
 import bannerImage from '../../images/banner.jpg'; // Atualize o caminho conforme necessário
 
 export const Container = styled.div`
-  padding: 20px;
+  padding: 0; /* Remove padding que pode afetar o banner */
   background-color: #5563e6;
   min-height: 100vh; /* Garante que o contêiner tenha pelo menos 100% da altura da viewport */
   
@@ -18,6 +17,8 @@ export const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
   color: white;
+  background:#003366;
+  height: 7vh; /* Ajusta a altura do nav */
 `;
 
 export const CartIcon = styled(FaShoppingCart)`
@@ -30,12 +31,15 @@ export const CartIcon = styled(FaShoppingCart)`
 `;
 
 export const Banner = styled.div`
-  width: 100%; /* Largura total da tela */
-  height:750px; /* Altura do banner */
+  width: 100vw; /* Largura total da viewport */
+  height: 750px; /* Altura do banner */
   background: url(${bannerImage}) no-repeat center center; /* Usar a imagem importada */
   background-size: cover; /* Faz com que a imagem cubra todo o banner */
   position: relative;
-  margin-bottom: 20px; /* Espaço entre o banner e o conteúdo abaixo */
+  margin: 0; /* Remove margens, se houver */
+  padding: 0; /* Remove padding, se houver */
+  box-sizing: border-box; /* Garante que o box model esteja correto */
+  overflow: hidden; /* Garante que nada saia dos limites do banner */
 `;
 
 export const SearchSection = styled.div`
@@ -47,8 +51,8 @@ export const SearchSection = styled.div`
   left: 50%;
   transform: translate(-50%, -50%); /* Centraliza o conteúdo dentro do banner */
   background-color: rgba(0, 0, 0, 0.7); /* Preto opaco */
-  padding: 20px;
-  border-radius: 8px; /* Opcional: Adiciona bordas arredondadas */
+  padding: 3px;
+  border-radius: 6px; /* Opcional: Adiciona bordas arredondadas */
   color: white;
   width: 50%; /* O input ocupará metade da largura do banner */
 `;
@@ -60,6 +64,7 @@ export const SearchInput = styled.input`
   border: 1px solid #ccc; /* Borda fina adicionada */
   border-radius: 4px;
   box-sizing: border-box; /* Garante que o padding não afete o tamanho total */
+  text-align: center; /* Centraliza o texto e o placeholder horizontalmente */
   
   &::placeholder {
     color: #ccc;
